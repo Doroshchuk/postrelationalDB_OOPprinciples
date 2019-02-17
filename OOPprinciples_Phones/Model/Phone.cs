9f189398194c461483ecfefcb95c602ed4d03ae6
Doroshchuk <doroshchukdaria@gmail.com>
2019-02-17 13:30:33 +0200

    created Phone abstract class

---
+++ OOPprinciples_Phones/Model/Phone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPprinciples_Phones.Model
+{
+    public abstract class Phone
+    {
+        private int _year;
+
+        public int Number { get; set; }
+
+        public Phone(int year)
+        {
+            _year = year;
+        }
+
+        public abstract void Call(int outputNumber);
+
+        public abstract void Ring(int inputNumber);
+    }
+}
