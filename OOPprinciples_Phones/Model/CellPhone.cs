bc769406ab9b664c0d6ece68d6f10dea1c3bb2c4
Doroshchuk <doroshchukdaria@gmail.com>
2019-02-17 13:51:08 +0200

    created WirelessPhone abstract class, CellPhone class

---
+++ OOPprinciples_Phones/Model/CellPhone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPprinciples_Phones.Model
+{
+    public class CellPhone : WirelessPhone
+    {
+        public CellPhone(int year, int hour) : base(year, hour)
+        {
+
+        }
+
+        public override void Call(int outputNumber)
+        {
+            Console.WriteLine("Осуществляется входящий вызов с номера '" + outputNumber + "' на текущий номер '" + Number + "'.");
+        }
+
+        public override void Ring(int inputNumber)
+        {
+            Console.WriteLine("Осуществляется исходящий вызов с текущего номера '" + Number + "' на номер другого абонента '" + inputNumber + "'.");
+        }
+    }
+}
---
+++ OOPprinciples_Phones/Model/WirelessPhone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPprinciples_Phones.Model
+{
+    public abstract class WirelessPhone : Phone
+    {
+        private int _hour;
+
+        public WirelessPhone(int year, int hour) : base(year)
+        {
+            _hour = hour;
+        }
+    }
+}
